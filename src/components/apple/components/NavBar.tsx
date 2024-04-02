dc81f59fafbdd2065c21ed96f50c94f48ad5b123
am1t-goswami <mern.amitkumar@gmail.com>
2024-04-02 21:04:57 +0530

    ref: code ref

---
+++ src/components/apple/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="assets/images/apple.svg" alt="logo" />
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
--- src/components/apple/index.tsx
+++ src/components/apple/index.tsx
@@ -1,3 +1,5 @@
+import { NavBar } from './components/NavBar'
+
 export const AppleComponent = () => {
-  return <div>Apple</div>
+  return <NavBar />
 }
